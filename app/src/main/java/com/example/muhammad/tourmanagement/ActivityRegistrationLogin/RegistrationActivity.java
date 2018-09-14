86172c09d4b013df6dabac065c6a6d63dae6fc7d
mdsharafat <sharafat.sohan047@gmail.com>
2018-09-14 22:34:18 +0600

    Project File Created

---
+++ app/src/main/java/com/example/muhammad/tourmanagement/ActivityRegistrationLogin/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.example.muhammad.tourmanagement.ActivityRegistrationLogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.muhammad.tourmanagement.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
